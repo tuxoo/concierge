347915f5600eb6a4877508a7bf1dac8a1ac6f440
Eugene <krivtsov.eugene@gmail.com>
2023-03-31 23:22:17 +0300

    Concierge: base cache has been added;

--- src/main/kotlin/ru/home/concierge/config/CacheConfig.kt
+++ src/main/kotlin/ru/home/concierge/config/CacheConfig.kt
@@ -1,7 +1,7 @@
 package ru.home.concierge.config
 
 import com.github.benmanes.caffeine.cache.Caffeine
-import org.springframework.cache.annotation.EnableCaching
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.util.concurrent.TimeUnit
@@ -12,4 +12,12 @@ class CacheConfig {
     @Bean
     fun cache(): Caffeine<Any, Any> =
         Caffeine.newBuilder().expireAfterWrite(60, TimeUnit.MINUTES)
+
+
+    @Bean
+    fun cacheManager(
+        caffeine: Caffeine<Any, Any>
+    ) = CaffeineCacheManager().apply {
+        this.setCaffeine(caffeine)
+    }
 }
